081c467b91b815b065552673005a62e0e4c412ea
SebasAll_Language <sebastiao.ramos900@gmail.com>
2025-04-15 21:55:30 +0100

    Style of The Page

---
+++ script.js
@@ -0,0 +1,9 @@
+function checkl(){
+    var up = document.getElementsByName
+    console.log(up);
+    var pass = document.getElementByPassword
+}
+
+if (up == 'root' && pass == 'root'){
+    alert('Login successful');
+}
