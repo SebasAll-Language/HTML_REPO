e166777d93f60da42ca70fa127b3818212746032
SebasAll_Language <sebastiao.ramos900@gmail.com>
2025-04-15 22:32:47 +0100

    The page

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function checkl(){
-    var up = document.getElementsByName;
-    console.log(up);
-    var pass = document.getElementByPassword;
-}
+function checkl(event) {
+    event.preventDefault();
+    var up = document.getElementsByName('username')[0].value;
+    var pass = document.getElementsByName('password')[0].value;
 
-if (up == 'root' && pass == 'root'){
-    alert('Login successful');
-    document.write('Login successful');
+    if (up === 'root' && pass === 'root') {
+        alert('Login successful');
+        document.body.innerHTML = '<h1>Login successful</h1>';
+    } else {
+        alert('Invalid credentials');
+        for (var i = 0; i < 17, i++){
+            alert('Invalid credentials');
+            document.body.innerHTML = '<h1>Invalid credentials</h1>';
+        }
+    }
 }
